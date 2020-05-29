a297f8138d00e980a34c1807bf442518374e8dea
tcbegley <tomcbegley@gmail.com>
2020-05-29 07:06:12 +0100

    Redesign layout to allow larger map

--- src/components/map/interactive-map.js
+++ src/components/map/interactive-map.js
@@ -1,23 +1,33 @@
 import React from 'react'
-import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
 
 import Card from './card'
 import Map from './map'
 
 const InteractiveMap = ({ films, currentFilm, setCurrentFilm }) => (
-  <Row className="my-3">
-    <Col className="mb-3 mb-lg-0" lg={8} xs={12}>
+  <div className="my-3 position-relative">
+    <div className="mb-3 mb-lg-0">
       <Map
         makeOnClick={film => () => setCurrentFilm(film)}
         films={films}
         currentFilm={currentFilm}
       />
-    </Col>
-    <Col lg={4} xs={12}>
+    </div>
+    <div
+      className="d-none d-lg-block position-absolute"
+      style={{
+        top: '20px',
+        right: '20px',
+        maxWidth: '300px',
+        width: '300px',
+        opacity: '95%',
+      }}
+    >
       <Card film={currentFilm} />
-    </Col>
-  </Row>
+    </div>
+    <div className="d-block d-lg-none">
+      <Card film={currentFilm} />
+    </div>
+  </div>
 )
 
 export default InteractiveMap
